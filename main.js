333022f6330420935cc7590ffc9cc5885990dcf7
pedro vinicius <128748644+pedrosantos-21@users.noreply.github.com>
2023-08-29 14:21:02 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,27 @@
-alert('Olá mundo');
-document.querySelector('.tecla_pom');
+function tocaSom(seletorAudio) {
+    document.querySelector(seletorAudio).play();
+}
+
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < ListaDeTeclas.length; contador++) {
+
+    const tecla = ListaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string  
+
+    tecla.onclick = function () {
+        tocaSom(idAudio)
+    };
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+}
